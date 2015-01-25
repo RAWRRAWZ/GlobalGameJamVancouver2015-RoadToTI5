a12d6bd688e6edea44fa8cc0d1a321e8ba4b1d89
RAWRRAWZ <ahung93@gmail.com>
2015-01-25 02:13:35 -0800

    Did some punching animations

---
+++ Project Shogun/Assets/punching.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class punching : MonoBehaviour {
+
+	public GameObject explosion;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Enemy" && transform.parent.GetComponent<PlayerAttack>().attack == true){
+			Instantiate(explosion,transform.position, Quaternion.identity);
+		}
+	}
+}
