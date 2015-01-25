d0318d3e83e77123c0873b867f1dc7b3b3a4b95d
William Zhang <william.zhang.285@gmail.com>
2015-01-24 18:46:51 -0800

    Adding lots of stuff!!!

---
+++ Project Shogun/Assets/Scripts/GemPickup.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemPickup : MonoBehaviour
+{
+	public AudioClip pickupClip;		// Sound for when the bomb crate is picked up.
+
+	private Animator anim;				// Reference to the animator component.
+	private bool landed = false;		// Whether or not the crate has landed yet.
+	private int superTime = 500;
+	private int gemThreshold = 1;
+
+	void Awake()
+	{	
+		// Setting up the reference.
+		anim = transform.root.GetComponent<Animator>();
+	}
+
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		// If the player enters the trigger zone...
+		if(other.tag == "Player")
+		{
+			// ... play the pickup sound effect.
+			AudioSource.PlayClipAtPoint(pickupClip, transform.position);
+
+			// Increase the number of bombs the player has.
+			PlayerState state = other.GetComponent<PlayerState>();
+
+			if (++state.numberOfGems >= gemThreshold) {
+				switch (state.currentState) {
+				case PlayerState.state.NINJA:
+				case PlayerState.state.SUPER_NINJA:
+					state.currentState = PlayerState.state.SUPER_NINJA;
+					state.superTime = superTime;
+					break;
+				case PlayerState.state.SAMURAI:
+				case PlayerState.state.SUPER_SAMURAI:
+					state.currentState = PlayerState.state.SUPER_SAMURAI;
+					state.superTime = superTime;
+					break;
+				default:
+					break;
+				}
+				state.numberOfGems = 0;
+			}
+
+			
+
+			// Destroy the crate.
+			Destroy(transform.root.gameObject);
+		}
+	}
+}
---
+++ Project Shogun/Assets/Scripts/SamuraiPickup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SamuraiPickup : MonoBehaviour
+{
+	public AudioClip pickupClip;		// Sound for when the bomb crate is picked up.
+	public PlayerState.state state;
+
+	private Animator anim;				// Reference to the animator component.
+	private bool landed = false;		// Whether or not the crate has landed yet.
+
+
+	void Awake()
+	{
+		// Setting up the reference.
+		anim = transform.root.GetComponent<Animator>();
+	}
+
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		// If the player enters the trigger zone...
+		if(other.tag == "Player")
+		{
+			// ... play the pickup sound effect.
+			AudioSource.PlayClipAtPoint(pickupClip, transform.position);
+
+			// Increase the number of bombs the player has.
+			other.GetComponent<PlayerState>().currentState = state;
+
+			// Destroy the crate.
+			Destroy(transform.root.gameObject);
+		}
+	}
+}
