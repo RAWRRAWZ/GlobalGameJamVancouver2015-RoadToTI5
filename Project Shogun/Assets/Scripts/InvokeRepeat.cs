0f76ccd0897ea2fd94fca28e3b6d8a8ad2bbed9d
npezarro <n.pezarro@alumni.ubc.ca>
2015-01-24 20:55:54 -0800

    Adding assets in

--- Project Shogun/Assets/Scripts/InvokeRepeat.cs
+++ Project Shogun/Assets/Scripts/InvokeRepeat.cs
@@ -14,8 +14,8 @@ public class InvokeRepeat : MonoBehaviour
 	{		// Instantiate a random enemy.
 		int targetIndex = Random.Range(0, targets.Length);
 		//Instantiate(targets[targetIndex], transform.position, transform.rotation);
-		Vector3 screenPosition = new Vector3 (0, 0, 0);
-		//Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0,Screen.width), Random.Range(0,Screen.height), Camera.main.farClipPlane/2));
+		//Vector3 screenPosition = new Vector3 (0, 0, 0);
+		Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0,Screen.width), Random.Range(0,Screen.height), Camera.main.farClipPlane/2));
 		Instantiate(targets[targetIndex], screenPosition, Quaternion.identity);
 	}
 }
