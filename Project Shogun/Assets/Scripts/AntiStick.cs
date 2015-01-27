2863d5b8895c5636c45ffb5e8d51710afe27c9c9
npezarro <n.pezarro@alumni.ubc.ca>
2015-01-26 20:42:01 -0800

    New Scene, hitbox and friction changes

---
+++ Project Shogun/Assets/Scripts/AntiStick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntiStick : MonoBehaviour {
+
+	private Transform StickCheck;			// A position marking where to check if the player is grounded.
+	private bool stuck = false;			// Whether or not the player is grounded.
+
+
+	void Awake()
+	{
+		// Setting up references.
+		StickCheck = transform.Find("antiStick");
+
+}
+
+
+void FixedUpdate()
+{
+				// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+				stuck = Physics2D.Linecast (transform.position, StickCheck.position, 1 << LayerMask.NameToLayer ("AntiStick"));  
+
+	// If the jump button is pressed and the player is grounded then the player should jump.
+		if  (stuck == true)
+		{
+			rigidbody2D.AddForce (new Vector2 (-10, -10));
+				//rigidbody2D.velocity = -rigidbody2D.velocity;
+		}
+	}
+}
+
+
+
+
