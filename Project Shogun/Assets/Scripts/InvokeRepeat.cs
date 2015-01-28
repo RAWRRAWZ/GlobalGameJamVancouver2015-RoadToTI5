920e3ace4ea9af3ea4af9696e079a35e34c3b1f8
RAWRRAWZ <ahung93@gmail.com>
2015-01-28 00:48:46 -0800

    Better collision detection at the price of sound spam for swords. Fixed spawn location in y axis

--- Project Shogun/Assets/Scripts/InvokeRepeat.cs
+++ Project Shogun/Assets/Scripts/InvokeRepeat.cs
@@ -21,9 +21,25 @@ public class InvokeRepeat : MonoBehaviour
 			int targetIndex = Random.Range (0, targets.Length);
 			//Instantiate(targets[targetIndex], transform.position, transform.rotation);
 			//Vector3 screenPosition = new Vector3 (0, 0, 0);
-
+			float y = -5f;
 			float x = Random.Range(-70.0f, 40.0f);
-			float y = Random.Range(-5.0f, -60.0f);
+			int randomSpawn = Random.Range (0,2);
+			switch(randomSpawn){
+				case 0:
+					y = Random.Range(-36f, -5.0f);
+					break;
+				case 1:
+					y = Random.Range(-52.0f, -42.0f);
+					break;
+				case 2:
+					y = Random.Range(-60.0f, -56.5f);
+					break;
+				default:
+					y = -5f;
+					break;
+			}
+
+
 			Instantiate(targets[targetIndex], new Vector2(x, y), Quaternion.identity);
 			//Vector2 screenPosition = Camera.main.ScreenToWorldPoint (new Vector3 (Random.Range (0, Screen.width-10), Random.Range (0, Screen.height-15), Camera.main.farClipPlane / 2));
 			//Instantiate (targets [targetIndex], screenPosition, Quaternion.identity);
--- Project Shogun/Assets/SwordHit.cs
+++ Project Shogun/Assets/SwordHit.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class SwordHit : MonoBehaviour {
-	
+
 	// Use this for initialization
 	void Start () {
 
@@ -23,5 +23,22 @@ public class SwordHit : MonoBehaviour {
 				}
 
 			}
+	}
+
+	void OnTriggerStay2D (Collider2D col) 
+	{
+		// If it hits an enemy...
+		if (col.tag == "Enemies" && transform.parent.GetComponent<Animator> ().GetCurrentAnimationClipState (0) [0].clip.name == "SwordCut") {
+			SoundManager.instance.Play ("SliceImpact");	
+			Destroy (col.gameObject);
+		} else if (col.gameObject.tag == "Player" && transform.parent.GetComponent<Animator> ().GetCurrentAnimationClipState (0) [0].clip.name == "SwordCut"){
+			SoundManager.instance.Play ("SliceImpact");
+			if (col.gameObject.GetComponent<PlayerState> ().currentState == PlayerState.modeState.NINJA)	{
+				SoundManager.instance.Play("SliceImpact");
+				col.gameObject.GetComponent<PlayerState> ().currentState--;
+				
+			}
+			
+		}
 	}
 }
