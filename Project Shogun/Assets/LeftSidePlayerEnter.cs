c93dead99aee22b4d33f5ead5ffc3ee9e8f504dd
RAWRRAWZ <ahung93@gmail.com>
2015-01-29 22:53:21 -0800

    Addd pacman walling

---
+++ Project Shogun/Assets/LeftSidePlayerEnter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftSidePlayerEnter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D (Collider2D col) {
+		// If it hits an enemy...
+		if (col.tag == "Enemies" || col.tag == "Player") {
+			col.transform.position = new Vector3(41.5f, col.transform.position.y, col.transform.position.z);
+		}
+	}
+}
